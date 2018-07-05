b09dc6410b2d6be114d799dd3c87c25406b4bedc
robby <0881londo@gmail.com>
2018-07-05 10:25:18 +0300

    initial commit

---
+++ tmp/cache/twigView/ce/ce29c080423a56b709b424dedf1a9f756278ff8a593c7703fa79502020d16cb5.php
@@ -0,0 +1,70 @@
+<?php
+
+/* C:\xampptpb\htdocs\cake\vendor\cakephp\bake\src\Template\Bake\Layout\default.twig */
+class __TwigTemplate_addb4138f712eaffaacd599e24bc178d02a06ed4bba36c2411af5b734318eb70 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $__internal_770edd655cdeb606afc443e4edb1f19b4248a91788cb82e88bf8b9495a7c5cfa = $this->env->getExtension("WyriHaximus\\TwigView\\Lib\\Twig\\Extension\\Profiler");
+        $__internal_770edd655cdeb606afc443e4edb1f19b4248a91788cb82e88bf8b9495a7c5cfa->enter($__internal_770edd655cdeb606afc443e4edb1f19b4248a91788cb82e88bf8b9495a7c5cfa_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "C:\\xampptpb\\htdocs\\cake\\vendor\\cakephp\\bake\\src\\Template\\Bake\\Layout\\default.twig"));
+
+        // line 16
+        echo $this->getAttribute((isset($context["_view"]) ? $context["_view"] : null), "fetch", array(0 => "content"), "method");
+        
+        $__internal_770edd655cdeb606afc443e4edb1f19b4248a91788cb82e88bf8b9495a7c5cfa->leave($__internal_770edd655cdeb606afc443e4edb1f19b4248a91788cb82e88bf8b9495a7c5cfa_prof);
+
+    }
+
+    public function getTemplateName()
+    {
+        return "C:\\xampptpb\\htdocs\\cake\\vendor\\cakephp\\bake\\src\\Template\\Bake\\Layout\\default.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  22 => 16,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("{#
+/**
+ * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link          http://cakephp.org CakePHP(tm) Project
+ * @since         2.0.0
+ * @license       http://www.opensource.org/licenses/mit-license.php MIT License
+ */
+#}
+{{ _view.fetch('content')|raw }}", "C:\\xampptpb\\htdocs\\cake\\vendor\\cakephp\\bake\\src\\Template\\Bake\\Layout\\default.twig", "C:\\xampptpb\\htdocs\\cake\\vendor\\cakephp\\bake\\src\\Template\\Bake\\Layout\\default.twig");
+    }
+}
